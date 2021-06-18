0105cb4d692d9def14874e4efcc475f9f5f1c1f7
Rich Lander <rlander@microsoft.com>
2021-06-17 21:42:43 -0700

    Add baseline to script

--- src/test.sh
+++ src/test.sh
@@ -6,8 +6,14 @@ TAG=bullseye-slim
 
 mkdir logs
 git clone --depth 1 https://github.com/dotnet/roslyn
-time docker run --rm -it  -v $(pwd)/logs:/logs -v $(pwd)/roslyn:/roslyn -w /roslyn mcr.microsoft.com/dotnet/sdk-composite:6.0-$TAG-$ARCH bash -c "dotnet build Compilers.sln > /logs/logs1.txt"
+time docker run --rm -it  -v $(pwd)/logs:/logs -v $(pwd)/roslyn:/roslyn -w /roslyn mcr.microsoft.com/dotnet/sdk-composite:6.0-$TAG-$ARCH bash -c "dotnet build Compilers.sln > /logs/logs-composite.txt"
 rm -rf roslyn
 git clone --depth 1 https://github.com/dotnet/roslyn
-time docker run --rm -it -v $(pwd)/logs:/logs -v $(pwd)/roslyn:/roslyn -w /roslyn -e COMPlus_TieredCompilation=0 mcr.microsoft.com/dotnet/sdk-composite:6.0-$TAG-$ARCH bash -c "dotnet build Compilers.sln > /logs/logs2.txt"
+time docker run --rm -it -v $(pwd)/logs:/logs -v $(pwd)/roslyn:/roslyn -w /roslyn -e COMPlus_TieredCompilation=0 mcr.microsoft.com/dotnet/sdk-composite:6.0-$TAG-$ARCH bash -c "dotnet build Compilers.sln > /logs/logs-composite-no-tc.txt"
+rm -rf roslyn
+git clone --depth 1 https://github.com/dotnet/roslyn
+time docker run --rm -it -v $(pwd)/logs:/logs -v $(pwd)/roslyn:/roslyn -w /roslyn mcr.microsoft.com/dotnet/sdk:6.0-$TAG-$ARCH bash -c "dotnet build Compilers.sln > /logs/logs-baseline.txt"
+rm -rf roslyn
+git clone --depth 1 https://github.com/dotnet/roslyn
+time docker run --rm -it -v $(pwd)/logs:/logs -v $(pwd)/roslyn:/roslyn -w /roslyn -e COMPlus_TieredCompilation=0 mcr.microsoft.com/dotnet/sdk:6.0-$TAG-$ARCH bash -c "dotnet build Compilers.sln > /logs/logs-baseline-no-tc.txt"
 rm -rf roslyn
