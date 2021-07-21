04e72d3fcde4b72d1b74cf430d098620699c66fd
Rich Lander <rlander@microsoft.com>
2021-07-21 10:51:17 -0700

    Update scripts

--- src/build.sh
+++ src/build.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-DOTNET_VERSION=6.0.0-preview.7.21316.3
+DOTNET_VERSION=6.0.0-rc.1.21370.5
 ARCH=amd64
 TAG=bullseye-slim
+FLAVOR=-foo
 
-docker build --pull --build-arg DOTNET_VERSION=$DOTNET_VERSION -t mcr.microsoft.com/dotnet/runtime-composite:6.0-$TAG-$ARCH runtime/6.0/$TAG/$ARCH/
-docker build --build-arg REPO=mcr.microsoft.com/dotnet/runtime-composite -t mcr.microsoft.com/dotnet/aspnet-composite:6.0-$TAG-$ARCH aspnet/6.0/$TAG/$ARCH/
-docker build --build-arg REPO=mcr.microsoft.com/dotnet/aspnet-composite -t mcr.microsoft.com/dotnet/sdk-composite:6.0-$TAG-$ARCH sdk/6.0/$TAG/$ARCH/
+docker build --pull --build-arg DOTNET_VERSION=$DOTNET_VERSION -t mcr.microsoft.com/dotnet/runtime$FLAVOR:6.0-$TAG-$ARCH runtime/6.0/$TAG/$ARCH/
+docker build --build-arg ASPNET_VERSION=$DOTNET_VERSION --build-arg REPO=mcr.microsoft.com/dotnet/runtime$FLAVOR -t mcr.microsoft.com/dotnet/aspnet$FLAVOR:6.0-$TAG-$ARCH aspnet/6.0/$TAG/$ARCH/
+docker build --build-arg REPO=mcr.microsoft.com/dotnet/aspnet$FLAVOR -t mcr.microsoft.com/dotnet/sdk$FLAVOR:6.0-$TAG-$ARCH sdk/6.0/$TAG/$ARCH/
